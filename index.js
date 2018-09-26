61ed323d2742567b4d58904166e3ce612940bea0
Juan Nicolas <jnma91@gmail.com>
2018-09-26 03:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(currentLine, newName){
   currentLine.push(newName);
   return `Welcome, ${newName}. You are number ${currentLine.length} in line.`
 }
+
+function nowServing(currentLine){
+  if (currentLine.length > 0){
+    return currentLine.shift();
+  } else {
+    return `The line is currently empty.`
+  }
+}
